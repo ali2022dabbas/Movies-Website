e91098962766aa6d6fb6d0fb2df12a7609fbc358
Ali <ali2022dabbas@gmail.com>
2023-12-08 01:27:37 +0300

    - HTML/CSS 60% complete - Base JS complete

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
- const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const API_URL = "https://api.tvmaze.com/shows";
 
- function getMovies() {
-    
- }
+const main = document.querySelector("main");
+
+async function getMovies() {
+  const res = await fetch(API_URL);
+  const resData = await res.json();
+
+  console.log(resData);
+
+  resData.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = ` <img src="${movie.image.medium}" alt="${movie.name}">  
+    <div class="movie-info">
+        <h3>${movie.name}</h3>
+        <span>${movie.rating.average}</span>
+    </div> `;
+
+    main.appendChild(movieEl);
+  });
+
+  return resData;
+}
+getMovies();
