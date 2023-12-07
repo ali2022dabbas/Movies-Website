d43c85b0a7399ad91ad6176bbc237b1076083a67
Ali <ali2022dabbas@gmail.com>
2023-12-08 02:09:45 +0300

    - completed rating color change based on rating

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const API_URL = "https://api.tvmaze.com/shows";
 const main = document.querySelector("main");
 
 async function getMovies() {
+    try {
   const res = await fetch(API_URL);
   const resData = await res.json();
 
@@ -11,15 +12,32 @@ async function getMovies() {
   resData.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
-    movieEl.innerHTML = ` <img src="${movie.image.medium}" alt="${movie.name}">  
+    movieEl.innerHTML = ` <img src="${movie.image.medium}" alt="${
+      movie.name
+    }">  
     <div class="movie-info">
         <h3>${movie.name}</h3>
-        <span>${movie.rating.average}</span>
+        <span class="${getClassByRate(movie.rating.average)}">
+        ${movie.rating.average}</span>
     </div> `;
 
     main.appendChild(movieEl);
   });
 
   return resData;
+} catch {
+    throw new Error("Oh no! There seems to be an error:")
 }
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
 getMovies();
